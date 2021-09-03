25098a237943536e47577903ebbaa15c52b46c92
abhinavpurohit47 <purohitabhinav01@gmail.com>
2021-09-03 22:03:10 +0530

    Change Theme removed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -72,13 +72,13 @@ const Sidebar = () => {
       >
         Email me
       </button>
-      <button
+      {/* <button
         onClick={changeTheme}
         className="w-8/12 px-5 py-2 my-4 text-white bg-black rounded-full cursor-pointer bg-gradient-to-r from-purple to-purple-dark focus:outline-none hover:scale-105 "
-      >
+      > */}
         {/* //TODO remove bg black */}
-        Change Theme
-      </button>
+        {/* Change Theme */}
+      {/* </button> */}
     </>
   );
 };
