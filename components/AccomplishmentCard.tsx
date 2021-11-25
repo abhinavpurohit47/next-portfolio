e8057eb2247227d1cf593181362131f0b8a9b312
abhinavpurohit47 <purohitabhinav01@gmail.com>
2021-11-25 22:58:37 +0530

    ✨ AccomplishmentCard created

---
+++ components/AccomplishmentCard.tsx
@@ -0,0 +1,118 @@
+import { FunctionComponent, useState } from "react";
+import { AiFillGithub, AiFillProject } from "react-icons/ai";
+import { MdClose } from "react-icons/md";
+import { IAccomplishments, IProject } from "../type";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { fadeInUp, stagger } from "../animations";
+// import Accomplishments from "../pages/accomplishments";
+
+const AccomplishmentCard: FunctionComponent<{
+  accomplishments: IAccomplishments;
+  showDetail: null | number;
+  setShowDetail: (id: null | number) => void;
+}> = ({
+  accomplishments: {
+    name,
+    image_path,
+    // category,
+    // deployed_url,
+    // description,
+    // github_url,
+    // key_techs,
+    id,
+  },
+}) => {
+  const [showDetail, setShowDetail] = useState(false);
+
+  return (
+    <div>
+      {/* image tag is used so as to improve the performance of the website */}
+      <Image
+        src={image_path}
+        alt={name}
+        className="cursor-pointer"
+        onClick={() => setShowDetail(true)}
+        layout="responsive"
+        height="150"
+        width="300"
+      />
+      {/* <img
+        src={image_path}
+        alt={name}
+        className="cursor-pointer"
+        onClick={() => setShowDetail(true)}
+      /> */}
+      <p className="my-2 text-center text-gray-900 dark:text-gray-300">
+        {name}
+      </p>
+
+      {showDetail && (
+        <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-black bg-gray-100 rounded-lg md:p-10 dark:bg-gray-800 dark:text-gray-400 md:grid-cols-2 gap-x-12 ">
+          <motion.div variants={stagger} initial="initial" animate="animate">
+            {/* <img src={image_path} alt={name} /> */}
+            <motion.div
+              variants={fadeInUp}
+              className="border-4 border-gray-100"
+            >
+              <Image
+                src={image_path}
+                alt={name}
+                layout="responsive"
+                height="150"
+                width="300"
+              />
+            </motion.div>
+            <motion.div className="flex justify-center my-4 space-x-3 text-gray-600">
+              <a
+                // href={github_url}
+                className="flex items-center px-4 py-2 space-x-3 text-lg text-gray-700 bg-gray-300 dark:bg-gray-600 rounded-xl dark:text-gray-300 "
+              >
+                <AiFillGithub /> <span>Github</span>
+              </a>
+              <a
+                // href={deployed_url}
+                className="flex items-center px-4 py-2 space-x-3 text-lg text-gray-700 bg-gray-300 dark:bg-gray-600 rounded-xl dark:text-gray-300 "
+              >
+                <AiFillProject /> <span>Project</span>
+              </a>
+            </motion.div>
+          </motion.div>
+
+          <motion.div variants={stagger} initial="initial" animate="animate">
+            <motion.h2
+              variants={fadeInUp}
+              className="mb-3 text-xl font-medium md:text-2xl"
+            >
+              {name}
+            </motion.h2>
+            <motion.h3 variants={fadeInUp} className="mb-3 font-medium">
+              {/* {description} */}
+            </motion.h3>
+
+            {/* <div className="flex flex-wrap mt-5 space-x-2 text-sm tracking-wider">
+              {key_techs.map((tech) => (
+                <span
+                  key={tech}
+                  className="px-2 py-1 my-1 text-gray-700 bg-gray-200 dark:text-gray-200 dark:bg-gray-600 rounded-xl "
+                >
+                  {tech}
+                </span>
+              ))}
+            </div> */}
+          </motion.div>
+
+          <button
+            onClick={() => setShowDetail(false)}
+            className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-gray-700"
+          >
+            <MdClose size={30} />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AccomplishmentCard;
